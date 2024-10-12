30e6cdde14df97ed3d330e2685713e7d01f22a6c
Sean Chan <sean.chan3@student.education.wa.edu.au>
2024-10-12 17:59:19 +0800

    added some animations for the menu and when the run is running, using the animation class in pybricks

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pybricks.pupdevices import Motor, Button
-from pybricks.parameters import Port
-from pybricks.tools import wait
+from pybricks.parameters import Port, Color, Icon
+from pybricks.tools import wait, Matrix, StopWatch
 from pybricks.hubs import PrimeHub
+
+hub = PrimeHub()
 
 def Pause(time:float):
     wait(time*1000)
@@ -13,6 +15,59 @@ class Motors:
     rightDrive = Motor(Port.B)
     big = Motor(Port.E)
     small = Motor(Port.F)
+
+class Animations:
+    running = [
+        Matrix([
+            [0, 0, 100, 100, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 100, 100, 0, 0]
+        ]), Matrix([
+            [100, 0, 0, 100, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 100, 0, 0, 100]
+        ]), Matrix([
+            [100, 100, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 100, 100]
+        ]), Matrix([
+            [100, 100, 100, 0, 0],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [0, 0, 100, 100, 100]
+        ]), Matrix([
+            [100, 100, 100, 100, 0],
+            [100, 0, 0, 0, 0],
+            [100, 0, 0, 0, 100],
+            [0, 0, 0, 0, 100],
+            [0, 100, 100, 100, 100]
+        ]), Matrix([
+            [100, 100, 100, 100, 100],
+            [100, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 100],
+            [100, 100, 100, 100, 100]
+        ]), Matrix([
+            [100, 100, 100, 100, 100],
+            [0, 0, 0, 0, 100],
+            [0, 0, 0, 0, 0],
+            [100, 0, 0, 0, 0],
+            [100, 100, 100, 100, 100]
+        ]), Matrix([
+            [0, 100, 100, 100, 100],
+            [0, 0, 0, 0, 100],
+            [100, 0, 0, 0, 100],
+            [100, 0, 0, 0, 0],
+            [100, 100, 100, 100, 0]
+        ])
+    ]
 
 def Forward(distance:float):
     Motors.leftDrive.run_angle(speed=distance, wait=False)
@@ -41,28 +96,41 @@ def run7():
     pass
 
 def DisplayNumber(number:int):
-    PrimeHub.display.off()
-    PrimeHub.display.number(number)
-    Pause(1)
+    hub.display.off()
+    hub.display.number(number)
+
+def StatusLight(color:Color):
+    hub.light.off()
+    hub.light.on(color)
 
 current_selection = 0
+
+StatusLight(Color.GREEN)
+hub.display.off()
+
 while True:
     while True:
-        if Button.LEFT in PrimeHub.buttons.pressed():
+        if Button.LEFT in hub.buttons.pressed():
             current_selection = (current_selection - 1) % 7
             break
 
-        elif Button.RIGHT in PrimeHub.buttons.pressed():
+        elif Button.RIGHT in hub.buttons.pressed():
             current_selection = (current_selection + 1) % 7
             break
 
-        elif Button.CENTER in PrimeHub.buttons.pressed():
+        elif Button.CENTER in hub.buttons.pressed():
             # run current selection
+            StatusLight(Color.YELLOW)
+            hub.display.animate(Animations.running, 30)
+            print(f"Running {current_selection + 1}...")
+            start_time = StopWatch.time()
             eval(f'run{str(current_selection + 1)}()')
+            print(f"Done running {current_selection + 1}. Time: {StopWatch.time() - start_time}")
+            StatusLight(Color.GREEN)
             break
     
     # wait until there are no buttons pressed
-    while not PrimeHub.buttons.pressed() == []:
+    while not hub.buttons.pressed() == []:
         Pause(0.01)
     
     # display current selection
